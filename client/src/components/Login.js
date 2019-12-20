c1a94b4ba5e8320d430489306132592938626b23
imxande <alerodriguezlarosa@gmail.com>
2019-12-20 14:27:39 -0500

    added delete and edit functions

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,7 +17,7 @@ const Login = (props) => {
          .then(res =>{
            console.log(res)
            localStorage.setItem('token', res.data.payload)
-           props.history.push('/colors')
+           props.history.push('/bubble')
          })
          .catch(err => console.log(err)) 
   }
